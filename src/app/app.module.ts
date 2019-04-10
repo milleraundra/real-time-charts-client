d9d932981aefeaeda2adb0b7bb38215051640052
Aundra Miller <miller.aundra@gmail.com>
2019-04-10 11:56:08 -0700

    Create a signal-r service to generate a connection to the signal-r hub and receive transferchartdata events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ChartsModule} from 'ng2-charts';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
